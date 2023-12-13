b347395bab972f1cf542b5b4a2d4be1b89faa9c9
yyyannnn <349168872@gapps.yrdsb.ca>
2023-12-13 08:37:58 -0500

    Added Images

---
+++ WhiteCircle.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class WhiteCircle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WhiteCircle extends Actor
+{
+    public WhiteCircle()
+    {
+       GreenfootImage image = new GreenfootImage("images/white circle.png");
+       image.scale(80,80);
+       setImage(image); 
+    }
+    
+    /**
+     * Act - do whatever the WhiteCircle wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
